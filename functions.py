4a525c6eb27795c8fcec86fa678eb31eb7018aa7
Jorimat <snedige@yahoo.com>
2026-09-17 11:43:04 +0200

    bes30 lukt nu beter

--- functions.py
+++ functions.py
@@ -51,13 +51,14 @@ def prepare_model(verbose=False):
     return predictor
     
 
+
 def make_mask(img, predictor, step = 500, visualize = False):
     
     # Generate query points for SAM
-    queries = list(itertools.product(
+    queries = np.array(list(itertools.product(
         np.arange(step//2, img.shape[1], step),
         np.arange(step//2, img.shape[0], step), 
-    ))
+    )))
     
     # Apply SAM2
     predictor.set_image(img)
@@ -74,12 +75,23 @@ def make_mask(img, predictor, step = 500, visualize = False):
     
         # Some selection
         for mask in masks:
+
+            if visualize:
+                plt.figure()
+                plt.subplot(121)
+                plt.imshow(img)
+                plt.scatter([input_point[0,0]], [input_point[0,1]])
+                plt.subplot(122)
+                plt.imshow(mask)
+                plt.show()
     
             # Size
             mask_size_min = 100000
             mask_size_max = 1000000
             mask_size = np.sum(mask)
             if mask_size < mask_size_min or mask_size > mask_size_max:
+                if visualize:
+                    print('Rejected because of size.')
                 break
         
             # Centeredness
@@ -87,41 +99,55 @@ def make_mask(img, predictor, step = 500, visualize = False):
             mask_cx_max = mask.shape[0]*0.75
             mask_cx = np.mean(np.where(mask)[0])
             if mask_cx < mask_cx_min or mask_cx > mask_cx_max:
+                if visualize:
+                    print('Rejected because of centeredness.')
                 break
             
-            mask_cy_min = mask.shape[1]*0.25
-            mask_cy_max = mask.shape[1]*0.75
-            mask_cy = np.mean(np.where(mask)[1])
-            if mask_cy < mask_cy_min or mask_cy > mask_cy_max:
-                break
+            # mask_cy_min = mask.shape[1]*0.25
+            # mask_cy_max = mask.shape[1]*0.75
+            # mask_cy = np.mean(np.where(mask)[1])
+            # if mask_cy < mask_cy_min or mask_cy > mask_cy_max:
+            #     if visualize:
+            #         print('Rejected because of centeredness.')
+            #     break
 
             # Outer dimensions
             mask_xrange_min = 500
             mask_xrange_max = 2000
             mask_xrange = max(np.where(mask)[0])-min(np.where(mask)[0])
             if mask_xrange < mask_xrange_min or mask_xrange > mask_xrange_max:
+                if visualize:
+                    print('Rejected because of outer dimensions.')
                 break
             
             mask_yrange_min = 500
             mask_yrange_max = 1500
             mask_yrange = max(np.where(mask)[1])-min(np.where(mask)[1])
             if mask_yrange < mask_yrange_min and mask_yrange > mask_yrange_max:
+                if visualize:
+                    print('Rejected because of outer dimensions.')
                 break
 
             # Greenness
-            mask_green_min = 0.8
+            # mask_green_min = 0.8
             mask_coords = np.array(np.where(mask)).transpose()
             mask_rgb = np.array([img[x, y] for x, y in  mask_coords])
-            mask_green = (mask_rgb[:,0] < mask_rgb[:,1]) & (mask_rgb[:,2] < mask_rgb[:,1])
+            # mask_green = (mask_rgb[:,0] < mask_rgb[:,1]) & (mask_rgb[:,2] < mask_rgb[:,1]) 
+            # mask_green = mask_rgb[:,0] < mask_rgb[:,1]
+            mask_green = 2*mask_rgb[:,1] - mask_rgb[:,0] - mask_rgb[:,2]
             mask_greenness = np.mean(mask_green)
-            if mask_greenness < mask_green_min:
-                break
+            # if mask_greenness < mask_green_min:
+            #     if visualize:
+            #         print(f'Rejected because of greenness: {mask_greenness}')
+            #     break
 
             # Filledness
-            fill_rate_min = 0.9
+            fill_rate_min = 0.8
             hull = convex_hull_image(mask)
             fill_rate = np.sum(hull & mask.astype(bool))/np.sum(hull)
             if fill_rate < fill_rate_min:
+                if visualize:
+                    print(f'Rejected because of filledness: {fill_rate}')
                 break
 
             # All test passed        
@@ -137,12 +163,16 @@ def make_mask(img, predictor, step = 500, visualize = False):
     # Best candidate has highest product of greenness and fill_rate
     if len(candidates) > 0:
         i_winner = np.argmax([(can['greenness']*can['fill_rate']) for can in candidates])
+        # i_winner = np.argmax([can['fill_rate'] for can in candidates])
         mask = candidates[i_winner]['mask']
         if visualize:
             plt.figure()
-            plt.subplot(121)
+            plt.subplot(131)
             plt.imshow(img)
-            plt.subplot(122)
+            plt.subplot(132)
+            plt.imshow(img)
+            plt.scatter(queries[:,0], queries[:,1])
+            plt.subplot(133)
             plt.imshow(mask)
             plt.show()
         return mask
@@ -151,13 +181,15 @@ def make_mask(img, predictor, step = 500, visualize = False):
         print('WARNING: No mask generated.')
         if visualize:
             plt.figure()
-            plt.subplot(121)
+            plt.subplot(131)
             plt.imshow(img)
-            plt.subplot(122)
-            plt.imshow(np.zeroes(img.shape))
+            plt.subplot(132)
+            plt.imshow(img)
+            plt.scatter(queries[:,0], queries[:,1])
+            plt.subplot(133)
+            plt.imshow(np.zeros(img.shape))
             plt.show()
         return None
-
 
 
 
@@ -213,9 +245,7 @@ def fit_ellipse(mask):
         "angle_deg": angle_deg,
     }
 
-
-
-def plot_mask_ellipse(mask, ellipse):
+def plot_mask_ellipse(mask, ellipse, ax):
 
     # plt.figure()
 
@@ -270,6 +300,63 @@ def plot_mask_ellipse(mask, ellipse):
     
     # plt.axis("equal")
     # plt.show()
+
+
+# def plot_mask_ellipse(mask, ellipse, ax):
+
+#     # plt.figure()
+
+#     # Mask
+#     plt.imshow(mask, origin="upper")
+    
+#     # Ellipse and center
+#     theta = np.linspace(0, 2*np.pi, 200)
+#     a = ellipse["semi_major"]
+#     b = ellipse["semi_minor"]
+#     cx, cy = ellipse["center"]
+#     angle = ellipse["angle_rad"]
+#     R = np.array([
+#         [np.cos(angle), -np.sin(angle)],
+#         [np.sin(angle),  np.cos(angle)]
+#     ])
+#     ellipse_xy = R @ np.vstack([
+#         a * np.cos(theta),
+#         b * np.sin(theta)
+#     ])
+#     ellipse_x = ellipse_xy[0] + cx
+#     ellipse_y = ellipse_xy[1] + cy
+#     plt.plot(ellipse_x, ellipse_y)
+#     plt.scatter(cx, cy, c="yellow")
+    
+#     # Major axis
+#     x = [
+#         ellipse['center'][0] - ellipse['semi_major']*ellipse['major_axis'][0],
+#         ellipse['center'][0] + ellipse['semi_major']*ellipse['major_axis'][0]
+#     ]
+#     y = [
+#         ellipse['center'][1] - ellipse['semi_major']*ellipse['major_axis'][1],
+#         ellipse['center'][1] + ellipse['semi_major']*ellipse['major_axis'][1]
+#     ]
+#     plt.plot(x,y)
+    
+#     # Minor axis
+#     x = [
+#         ellipse['center'][0] - ellipse['semi_minor']*ellipse['minor_axis'][0],
+#         ellipse['center'][0] + ellipse['semi_minor']*ellipse['minor_axis'][0]
+#     ]
+#     y = [
+#         ellipse['center'][1] - ellipse['semi_minor']*ellipse['minor_axis'][1],
+#         ellipse['center'][1] + ellipse['semi_minor']*ellipse['minor_axis'][1]
+#     ]
+#     plt.plot(x,y)
+    
+#     # Zoom box
+#     margin = 50
+#     plt.ylim(max(np.where(mask)[0])+margin, min(np.where(mask)[0])-margin)
+#     plt.xlim(min(np.where(mask)[1])-margin, max(np.where(mask)[1])+margin)
+    
+#     # plt.axis("equal")
+#     # plt.show()
 
 
 def find_grid_spacing(img, visualize=False):
